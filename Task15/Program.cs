07870f9e316fb1dfa55bfbb9959dfe9dced206f1
Ageev Ilya <Aii23@mail.ru>
2023-10-15 22:55:33 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7 ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 0 && num < 8 )
+{
+    if(num > 5 )
+    {
+      Console.Write("Yes");  
+    }
+    else
+    {
+      Console.Write("No");
+    }
+}
+else
+{
+    Console.Write("Такого дня недели не существует");
+}
