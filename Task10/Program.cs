e47c74e93616bef71c331d8b6406f70933096afd
Ageev Ilya <Aii23@mail.ru>
2023-10-17 20:03:46 +0300

    Task10_1

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите трёхзначное число - ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number > 99 && number < 1000 )
+if ((number > 99 && number < 1000) || (number < -99 && number > -1000))
 {
    int result = number % 100 / 10;
    Console.Write("Вторая цифра этого числа " + result);
