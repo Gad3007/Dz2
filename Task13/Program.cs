8126781922c7b77ced86145aa19be9357780f3d1
Ageev Ilya <Aii23@mail.ru>
2023-10-17 20:02:01 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99 || number < -99)
+{
+    int result = number % 1000 / 100;
+    Console.Write(result);
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+} 
